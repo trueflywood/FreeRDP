79da8ce781f5e97c4e49e62f1edf97564a067ddc
Kompanets Alexey <a.kompanec@scloud.ru>
2024-12-06 16:56:42 +0300

    fix: x86_64

--- scripts/android-build-ffmpeg.sh
+++ scripts/android-build-ffmpeg.sh
@@ -165,7 +165,7 @@ do
             ARCH_OPTIONS="	--disable-neon --disable-asm --disable-inline-asm"
         ;;
         x86_64)
-            TARGET_CPU="x86_64"
+            TARGET_CPU="x86-64"
             TARGET_ARCH="x86_64"
             ARCH_OPTIONS="	--disable-neon --enable-asm --enable-inline-asm"
         ;;
